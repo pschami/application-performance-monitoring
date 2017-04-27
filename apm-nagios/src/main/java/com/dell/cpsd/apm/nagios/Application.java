ab9ff8f5aed650793b91d119df12bdc68839159d
Barry,Patrick <Patrick.Barry@vce.com>
2017-04-27 16:52:07 +0100

    update application event and generate same alert 5 times before new one

--- apm-nagios/src/main/java/com/dell/cpsd/apm/nagios/Application.java
+++ apm-nagios/src/main/java/com/dell/cpsd/apm/nagios/Application.java
@@ -1,18 +1,12 @@
 package com.dell.cpsd.apm.nagios; /**
- * Copyright &copy; 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
- */
+									* Copyright &copy; 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
+									*/
 
-import com.dell.cpsd.apm.nagios.api.ApplicationPerformanceEvent;
-import com.dell.cpsd.apm.nagios.producer.ApplicationPerformanceProducer;
-import com.dell.cpsd.apm.nagios.services.ApplicationPerformanceException;
-import com.dell.cpsd.hdp.capability.registry.client.binding.config.CapabilityRegistryBindingManagerConfig;
-
+import java.util.Date;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.transaction.RabbitTransactionManager;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -21,10 +15,15 @@ import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import com.dell.cpsd.apm.nagios.api.ApplicationPerformanceEvent;
+import com.dell.cpsd.apm.nagios.producer.ApplicationPerformanceProducer;
+import com.dell.cpsd.apm.nagios.services.ApplicationPerformanceException;
+import com.dell.cpsd.hdp.capability.registry.client.binding.config.CapabilityRegistryBindingManagerConfig;
+
 /**
  * Spring boot application class
  * <p>
- * Copyright &copy; 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
+ * Copyright &copy; 2017 Dell Inc. or its subsidiaries. All Rights Reserved.
  * </p>
  *
  * @version 1.0
@@ -33,29 +32,46 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 @Import(CapabilityRegistryBindingManagerConfig.class)
-public class Application extends AsyncConfigurerSupport
-{
-    public static void main(String[] args) throws Exception
-    {
-      
-    	ApplicationContext applicationContext = new SpringApplicationBuilder().sources(Application.class).bannerMode(Banner.Mode.LOG).run(args);
-    	
-    	final ApplicationPerformanceProducer producer = applicationContext.getBean(ApplicationPerformanceProducer.class);
-    	    		
-    	Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
+public class Application extends AsyncConfigurerSupport {
+	
+	public static int count = 0;
+	public static UUID uuid = UUID.randomUUID();
+	
+	public static void main(String[] args) throws Exception {
+
+		ApplicationContext applicationContext = new SpringApplicationBuilder().sources(Application.class)
+				.bannerMode(Banner.Mode.LOG).run(args);
+
+		final ApplicationPerformanceProducer producer = applicationContext
+				.getBean(ApplicationPerformanceProducer.class);
+
+		
 			
+		Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
+
 			@Override
 			public void run() {
-				try {
-					producer.publishApplicationPerformanceEvent(new ApplicationPerformanceEvent("test alert......"));
+				try {				
+					count++;				
+					
+					ApplicationPerformanceEvent event = new ApplicationPerformanceEvent("high",
+							uuid.toString(), new Date(), "patricks-pc", "the message from nagios");
+
+					producer.publishApplicationPerformanceEvent(event);
+					
+					if (count > 5) {
+						count = 0;
+						uuid = UUID.randomUUID();						
+					}						
+					
 				} catch (ApplicationPerformanceException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
+			
 		}, 5, 5, TimeUnit.SECONDS);
-    }
+	}
 
 }
-
